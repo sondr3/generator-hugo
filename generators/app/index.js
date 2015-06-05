3bc4a9471ff4c1d1cc9ceb35058024935e946ea8
Sondre Nilsen <nilsen.sondre@gmail.com>
2015-06-05 19:37:06 +0200

    Updated main app generator

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,14 +1,127 @@
 'use strict';
 
+var _ = require('lodash');
+var chalk = require('chalk');
 var generator = require('yeoman-generator');
 
 module.exports = generator.Base.extend({
+  initializing: function() {
+    this.props = {};
+    this.pkg = this.fs.readJSON(this.destinationPath('package.json'), {});
+  },
+
+  prompting: function() {
+    var done = this.async();
+
+    var prompts = [{
+      name: 'projectName',
+      message: 'What is the name of your project?',
+      store: true
+    }, {
+      name: 'projectDescription',
+      message: 'Describe your project',
+      store: true
+    }, {
+      name: 'projectURL',
+      message: chalk.red('If you are using GHPages use username.github.io') +
+        '\nWhat will the URL for your project be?',
+      store: true
+    }, {
+      name: 'authorName',
+      message: 'What\'s your name?',
+      store: true
+    }, {
+      name: 'authorEmail',
+      message: 'What\'s your email?',
+      store: true
+    }, {
+      name: 'authorBio',
+      message: 'Write a short description about yourself',
+      store: true
+    }, {
+      name: 'authorTwitter',
+      message: 'Your Twitter handle',
+      store: true,
+    }, {
+      name: 'uploading',
+      type: 'list',
+      message: 'How do you want to upload your site?',
+      choices: ['Amazon S3', 'Rsync', 'Github Pages', 'None'],
+      store: true
+    }, {
+      name: 'permalinks',
+      type: 'list',
+      message: 'Permalink style' + (chalk.red(
+                     '\n  pretty: /:year/:month/:day/:title/' +
+                     '\n  date:   /:year/:month/:day/:title.html' +
+                     '\n  none:   /:categories/:title.html')) + '\n',
+      choices: ['pretty', 'date', 'none'],
+      store: true
+    }, {
+      name: 'pagination',
+      message: 'How many posts do you want to show on your front page?' +
+        chalk.red('\nMust be a number or all'),
+      store: true,
+      default: 10,
+      validate: function(input) {
+        if (/^[0-9]*$/.test(input)) {
+          return true;
+        }
+        if (/^all*$/i.test(input)) {
+          return true;
+        }
+        return 'Must be a number or all';
+      }
+    }];
+
+    this.prompt(prompts, function(props) {
+      this.props = _.extend(this.props, props);
+
+      done();
+    }.bind(this));
+  },
+
+  writing: function() {
+    var pkgJSONFields = {
+      name: _.kebabCase(this.props.projectName),
+      version: '0.0.0',
+      description: this.props.projectDescription,
+      homepage: this.props.projectURL,
+      author: {
+        name: this.props.authorName,
+        email: this.props.authorEmail
+      }
+    };
+
+    this.fs.writeJSON('package.json', _.extend(pkgJSONFields, this.pkg));
+  },
+
   default: function() {
     this.composeWith('hugo:dotfiles', {}, {
       local: require.resolve('../dotfiles')
     });
 
-    this.composeWith('hugo:hugo', {}, {
+    this.composeWith('hugo:gulp', {
+      options: {
+        uploading: this.props.uploading
+      }
+    }, {
+      local: require.resolve('../gulp')
+    });
+
+    this.composeWith('hugo:hugo', {
+      options: {
+        projectName: this.props.projectName,
+        projectDescription: this.props.projectDescription,
+        projectURL: this.props.projectURL,
+        authorName: this.props.authorName,
+        authorEmail: this.props.authorEmail,
+        authorBio: this.props.authorBio,
+        authorTwitter: this.props.authorTwitter,
+        permalinks: this.props.permalinks,
+        pagination: this.props.pagination
+      }
+    }, {
       local: require.resolve('../hugo')
     });
   },
--- test/app.js
+++ test/app.js
@@ -1,14 +1,42 @@
 'use strict';
 
 var path = require('path');
+var fs = require('fs');
 var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
 
+function assertObjectContains(obj, content) {
+  Object.keys(content).forEach(function(key) {
+    if (typeof content[key] === 'object') {
+      assertObjectContains(content[key], obj[key]);
+    } else {
+      assert.equal(content[key], obj[key]);
+    }
+  });
+}
+
+function assertJSONFileContains(filename, content) {
+  var obj = JSON.parse(fs.readFileSync(filename, 'utf8'));
+  assertObjectContains(obj, content);
+}
+
 describe('hugo:app', function() {
   before(function(done) {
+    this.answers = {
+      projectName: 'hugo',
+      projectDescription: 'Test site for hugo',
+      projectURL: 'www.test.com',
+      authorName: 'Ola Nordmann',
+      authorEmail: 'ola.nordmann@gmail.com',
+      authorBio: 'A norwegian dude',
+      authorTwitter: '0lanordmann',
+      uploading: 'None',
+      permalinks: 'pretty',
+      pagination: '10'
+    };
     helpers.run(path.join(__dirname, '../generators/app'))
-      .withOptions({skipInstall: true})
-      .withPrompts({someOption: true})
+      .inDir(path.join(__dirname, 'tmp/app'))
+      .withPrompts(this.answers)
       .on('end', done);
   });
 
@@ -20,7 +48,23 @@ describe('hugo:app', function() {
       '.jscsrc',
       '.jshintrc',
       'config.toml',
-      'config.prod.toml'
+      'config.prod.toml',
+      'gulpfile.js',
+      'package.json'
     ]);
+  });
+
+  it('creates package.json', function() {
+    assert.file('package.json');
+    assertJSONFileContains('package.json', {
+      name: this.answers.projectName,
+      version: '0.0.0',
+      description: this.answers.projectDescription,
+      homepage: this.answers.projectURL,
+      author: {
+        name: this.answers.authorName,
+        email: this.answers.authorEmail
+      },
+    });
   });
 });
