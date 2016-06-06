69749aee9f83973b29e1dc830213db3e6d51329c
Sondre Nilsen <nilsen.sondre@gmail.com>
2016-06-07 01:06:55 +0200

    Remove yet another unneeded file

--- index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = {
-  'app': require.resolve('./generators/app'),
-  'dotfiles': require.resolve('./generators/dotfiles'),
-  'hugo': require.resolve('./generators/hugo')
-};
